a74bd603d94d61fa2567f21b974d249de70d14d7
Johannes Chorzempa <jchorzempa@posteo.org>
2022-11-16 22:01:52 +0100

    Add JS30 sticky nav codebase, adjust, add bg img

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const sideMenuButton = document.querySelector('.sidemenu_button');
 const sideBar = document.querySelector('.sidebar');
 const list = sideBar.classList;
+const nav = document.querySelector('.nav');
 
 function toggleSideMenu() {
 	list.toggle('display__there');
 }
 
 sideMenuButton.addEventListener('click', toggleSideMenu);
+
+let topOfNav = nav.offsetTop;
+
+function fixNav() {
+	if (window.scrollY >= topOfNav) {
+		document.body.style.paddingTop = nav.offsetHeight + 'px';
+		document.body.classList.add('fixed-nav');
+	} else {
+		document.body.classList.remove('fixed-nav');
+		document.body.style.paddingTop = 0;
+	}
+}
+
+window.addEventListener('scroll', fixNav);
