48a7c31bb373f5787785a2b1f7e8fb66717131e8
Johannes Chorzempa <jchorzempa@posteo.org>
2022-11-25 14:31:36 +0100

    Edit project as typescript

---
+++ script.ts
@@ -0,0 +1,43 @@
+const sideMenuButton = document.querySelector('.sidemenu_button') as HTMLElement;
+const sideBar = document.querySelector('.sidebar') as HTMLElement;
+const list = sideBar.classList;
+const nav = document.querySelector('.nav') as HTMLDivElement;
+const headerLogo = document.querySelector('#header__logo') as HTMLImageElement;
+
+function toggleSideMenu() {
+	list.toggle('display__there');
+}
+
+sideMenuButton.addEventListener('click', toggleSideMenu);
+
+const topOfNav = nav.offsetTop;
+
+// function shrinkLogo() {
+// 	headerLogo.classList.toggle('shrink');
+// }
+
+function fixNav() {
+	if (window.scrollY >= topOfNav) {
+		document.body.style.paddingTop = `${nav.offsetHeight}px`;
+		document.body.classList.add('fixed-nav');
+	} else {
+		document.body.classList.remove('fixed-nav');
+		document.body.style.paddingTop = '0';
+	}
+}
+
+function scrollFunction() {
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+		headerLogo.style.height = '50px';
+	} else {
+		headerLogo.style.height = '200px';
+	}
+}
+
+window.addEventListener('scroll', () => {
+	if (window.innerWidth > 700) {
+		fixNav();
+	}
+});
+
+window.addEventListener('scroll', scrollFunction);
