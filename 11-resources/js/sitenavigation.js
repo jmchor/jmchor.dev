5fea19dbb28a001e9c42e05174ead7f3e79710a2
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-28 17:15:58 +0100

    Switch scrollToMain to scrollIntoView()

--- 11-resources/js/sitenavigation.js
+++ 11-resources/js/sitenavigation.js
@@ -1,9 +1,11 @@
 "use strict";
 const anchorLinkMain = document.getElementById('down');
+const goToHere = document.getElementById('anchor-2');
 function scrollToMain() {
-    window.scrollTo({
-        top: 1650,
-        behavior: 'smooth',
-    });
+    // window.scrollTo({
+    // 	top: window.innerHeight + 405,
+    // 	behavior: 'smooth',
+    // });
+    goToHere.scrollIntoView({ block: 'nearest', behavior: 'smooth' });
 }
 anchorLinkMain.addEventListener('click', scrollToMain);
--- 11-resources/ts/sitenavigation.ts
+++ 11-resources/ts/sitenavigation.ts
@@ -1,10 +1,13 @@
 const anchorLinkMain = document.getElementById('down') as HTMLButtonElement;
+const goToHere = document.getElementById('anchor-2') as HTMLDivElement;
 
 function scrollToMain(): void {
-	window.scrollTo({
-		top: 1650,
-		behavior: 'smooth',
-	});
+	// window.scrollTo({
+	// 	top: window.innerHeight + 405,
+	// 	behavior: 'smooth',
+	// });
+
+	goToHere.scrollIntoView({ block: 'nearest', behavior: 'smooth' });
 }
 
 anchorLinkMain.addEventListener('click', scrollToMain);
